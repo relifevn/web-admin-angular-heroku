33822d083605465d057f427c5d7d4b2116357835
Thinh Huynh <thinhhuynh3108@gmail.com>
2021-10-14 20:26:14 +0700

    Refactor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,74 +4,75 @@ import { FLAME_STATUS } from 'src/app/core/constants'
 import { SocketService } from 'src/app/core/services'
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+	flameStatus: FLAME_STATUS = FLAME_STATUS.OFF
+	cameraRaw: SafeUrl = null
+	cameraFilter: SafeUrl = null
 
-  flameStatus: FLAME_STATUS = FLAME_STATUS.OFF
-  cameraRaw: SafeUrl = null
-  cameraFilter: SafeUrl = null
+	constructor(
+		private socketService: SocketService,
+		private sanitization: DomSanitizer,
+	) {
+		this.socketService.temperatureData$.subscribe(data => {
+			this.chartDatasets[0].data = [
+				...this.chartDatasets[0].data,
+				...data.data.map(e => e.value),
+			]
+			const length = this.chartDatasets[0].data.length
+			if (length > 20) {
+				this.chartDatasets[0].data = this.chartDatasets[0].data.slice(
+					length - 20,
+					length,
+				)
+			}
+			this.chartLabels = this.chartDatasets[0].data.map((e, i) => String(i))
+		})
+		this.socketService.cameraRaw$.subscribe(img => {
+			this.cameraRaw = this.sanitization.bypassSecurityTrustUrl(img)
+		})
+		this.socketService.cameraFilter$.subscribe(img => {
+			this.cameraFilter = this.sanitization.bypassSecurityTrustUrl(img)
+		})
+		this.socketService.detectFlame$.subscribe(data => {
+			if (data === 1) {
+				this.flameStatus = FLAME_STATUS.ON
+			} else {
+				this.flameStatus = FLAME_STATUS.OFF
+			}
+		})
+	}
 
-  constructor(
-    private socketService: SocketService,
-    private sanitization: DomSanitizer,
-  ) {
-    this.socketService.temperatureData$.subscribe(data => {
-      this.chartDatasets[0].data = [
-        ...this.chartDatasets[0].data,
-        ...data.data.map(e => e.value)
-      ]
-      const length = this.chartDatasets[0].data.length
-      if (length > 20) {
-        this.chartDatasets[0].data = this.chartDatasets[0].data.slice(length - 20, length)
-      }
-      this.chartLabels = this.chartDatasets[0].data.map((e, i) => String(i))
-    })
-    this.socketService.cameraRaw$.subscribe(img => {
-      this.cameraRaw = this.sanitization.bypassSecurityTrustUrl(img)
-    })
-    this.socketService.cameraFilter$.subscribe(img => {
-      this.cameraFilter = this.sanitization.bypassSecurityTrustUrl(img)
-    })
-    this.socketService.detectFlame$.subscribe(data => {
-      if (data === 1) {
-        this.flameStatus = FLAME_STATUS.ON
-      } else {
-        this.flameStatus = FLAME_STATUS.OFF
-      }
-    })
-  }
+	chartType = 'line'
 
-  public chartType = 'line'
+	chartDatasets: Array<{ data: number[]; label: string }> = [
+		{
+			data: [],
+			label: 'Temperature',
+		},
+	]
 
-  public chartDatasets: Array<{ data: number[], label: string }> = [
-    {
-      data: [],
-      label: 'Temperature',
-    },
-  ]
+	chartLabels: Array<string> = []
 
-  public chartLabels: Array<string> = []
+	chartColors: Array<any> = [
+		{
+			backgroundColor: 'rgba(105, 0, 132, .2)',
+			borderColor: 'rgba(200, 99, 132, .7)',
+			borderWidth: 2,
+		},
+		// {
+		//   backgroundColor: 'rgba(0, 137, 132, .2)',
+		//   borderColor: 'rgba(0, 10, 130, .7)',
+		//   borderWidth: 2,
+		// }
+	]
 
-  public chartColors: Array<any> = [
-    {
-      backgroundColor: 'rgba(105, 0, 132, .2)',
-      borderColor: 'rgba(200, 99, 132, .7)',
-      borderWidth: 2,
-    },
-    // {
-    //   backgroundColor: 'rgba(0, 137, 132, .2)',
-    //   borderColor: 'rgba(0, 10, 130, .7)',
-    //   borderWidth: 2,
-    // }
-  ]
-
-  public chartOptions: any = {
-    responsive: true
-  }
-  public chartClicked(e: any): void { }
-  public chartHovered(e: any): void { }
-
+	chartOptions: any = {
+		responsive: true,
+	}
+	chartClicked(e: any): void {}
+	chartHovered(e: any): void {}
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,25 +1,24 @@
 import { CommonModule } from '@angular/common'
-import { HomeComponent } from './home.component'
 import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 import { ChartsModule, WavesModule } from 'angular-bootstrap-md'
 import { SocketService } from 'src/app/core/services'
+
 import { LottieAppModule } from '../lottie/lottie.module'
+import { HomeComponent } from './home.component'
 
 @NgModule({
-  declarations: [HomeComponent],
-  providers: [
-    SocketService,
-  ],
-  imports: [
-    RouterModule.forChild([{ path: '', component: HomeComponent }]),
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ChartsModule,
-    WavesModule,
-    LottieAppModule,
-  ],
+	declarations: [HomeComponent],
+	providers: [SocketService],
+	imports: [
+		RouterModule.forChild([{ path: '', component: HomeComponent }]),
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule,
+		ChartsModule,
+		WavesModule,
+		LottieAppModule,
+	],
 })
-export class HomeModule { }
+export class HomeModule {}
